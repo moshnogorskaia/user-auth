320420ee57930da95b6d7f99339d16170ab5ba7b
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-11-28 08:57:22 +0100

    Validate user input

---
+++ actions/auth-actions.js
@@ -0,0 +1,23 @@
+'use server';
+
+export async function signup(prevState, formData) {
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  const errors = {};
+
+  if (!email || !email.includes('@')) {
+    errors.email = 'Invalid email address';
+  }
+
+  if (!password || password.length < 8) {
+    errors.password = 'Password must be at least 8 characters long';
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
+  const user = await db.insert('users').values({ email, password }).returning();
+  return user;
+}
