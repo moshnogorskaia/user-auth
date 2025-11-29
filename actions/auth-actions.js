84eb79c8f66da981807aa5a4069e1a8181f19d05
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-11-29 09:37:52 +0100

    Hash the user password

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,24 +1,26 @@
-'use server';
+"use server";
 
-import { createUser } from '@/lib/user';
+import { createUser } from "@/lib/user";
+import { hashUserPassword } from "@/lib/hash";
 
 export async function signup(prevState, formData) {
-  const email = formData.get('email');
-  const password = formData.get('password');
+  const email = formData.get("email");
+  const password = formData.get("password");
 
   const errors = {};
 
-  if (!email || !email.includes('@')) {
-    errors.email = 'Invalid email address';
+  if (!email || !email.includes("@")) {
+    errors.email = "Invalid email address";
   }
 
   if (!password || password.length < 8) {
-    errors.password = 'Password must be at least 8 characters long';
+    errors.password = "Password must be at least 8 characters long";
   }
 
   if (Object.keys(errors).length > 0) {
     return { errors };
   }
 
-  createUser(email, password);
+  const hashedPassword = hashUserPassword(password);
+  createUser(email, hashedPassword);
 }
